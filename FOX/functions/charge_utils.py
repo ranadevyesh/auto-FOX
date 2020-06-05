12636b8490b54d343c81582a0bf55616eea6ad1c
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-05 14:49:39 +0200

    WiP
    * Ensure that `ChargeError` instances can be pickled.
    * Cleaned up type hints.
    * WiP: Addition of the `ChargeMapping` class.

--- FOX/functions/charge_utils.py
+++ FOX/functions/charge_utils.py
@@ -12,29 +12,47 @@ API
 
 """
 
-import functools
+from functools import partial
 from types import MappingProxyType
 from typing import (
     Hashable, Optional, Collection, Mapping, Container, Dict, Union, Iterable, Tuple, Any,
-    SupportsFloat, Generator, Iterator, Set
+    SupportsFloat, Generator, Iterator, Set, TypeVar, overload, ItemsView, ValuesView, KeysView,
+    FrozenSet, Type, Generic
 )
 
 import numpy as np
 import pandas as pd
 
+from nanoutils import TypedDict
+
+from ..type_hints import ArrayLike
+
 __all__ = ['update_charge']
 
-ConstrainDict = Mapping[Hashable, functools.partial]
+T = TypeVar('T')
+KT = TypeVar('KT', bound=Hashable)
+ST = TypeVar('ST', bound='ChargeError')
 
 
-class ChargeError(ValueError):
-    """A :exc:`ValueError` subclass for charge-related errors."""
+class _StateDict(TypedDict):
+    """A dictionary representing the keyword-only arguments of :exc:`ChargeError`."""
 
     reference: Optional[float]
     value: Optional[float]
     tol: Optional[float]
 
-    def __init__(self, *args: Any, reference: Optional[SupportsFloat] = None,
+
+class ChargeError(ValueError, Generic[T]):
+    """A :exc:`ValueError` subclass for charge-related errors."""
+
+    __slots__ = ('__weakref__', 'reference', 'value', 'tol')
+
+    reference: Optional[float]
+    value: Optional[float]
+    tol: Optional[float]
+    args: Tuple[T, ...]
+
+    def __init__(self, *args: T, reference: Optional[SupportsFloat] = None,
                  value: Optional[SupportsFloat] = None,
                  tol: Optional[SupportsFloat] = 0.001) -> None:
         """Initialize an instance."""
@@ -42,6 +60,17 @@ class ChargeError(ValueError):
         self.reference = float(reference) if reference is not None else None
         self.value = float(value) if value is not None else None
         self.tol = float(tol) if tol is not None else None
+
+    def __reduce__(self: ST) -> Tuple[Type[ST], Tuple[T, ...], _StateDict]:
+        """Helper for :mod:`pickle`."""
+        cls = type(self)
+        kwargs = _StateDict(reference=self.reference, value=self.value, tol=self.tol)
+        return cls, self.args, kwargs
+
+    def __setstate__(self, state: _StateDict) -> None:
+        """Helper for :meth:`pickle`; handles the setting of keyword arguments."""
+        for k, v in state.items():
+            setattr(self, k, v)
 
 
 def get_net_charge(param: pd.Series, count: pd.Series,
@@ -74,10 +103,10 @@ def get_net_charge(param: pd.Series, count: pd.Series,
     return ret.sum()
 
 
-def update_charge(atom: Hashable, value: float, param: pd.Series, count: pd.Series,
-                  constrain_dict: Optional[ConstrainDict] = None,
-                  prm_min: Optional[Iterable[float]] = None,
-                  prm_max: Optional[Iterable[float]] = None,
+def update_charge(atom: KT, value: float, param: pd.Series, count: pd.Series,
+                  constrain_dict: Optional[Mapping[KT, partial]] = None,
+                  prm_min: Optional[ArrayLike] = None,
+                  prm_max: Optional[ArrayLike] = None,
                   net_charge: Optional[float] = None) -> Optional[ChargeError]:
     """Set the atomic charge of **at** equal to **charge**.
 
@@ -135,8 +164,8 @@ def update_charge(atom: Hashable, value: float, param: pd.Series, count: pd.Seri
     return None
 
 
-def constrained_update(at1: Hashable, param: pd.Series,
-                       constrain_dict: Optional[ConstrainDict] = None) -> Set[Hashable]:
+def constrained_update(at1: KT, param: pd.Series,
+                       constrain_dict: Optional[Mapping[KT, partial]] = None) -> Set[KT]:
     """Perform a constrained update of atomic charges.
 
     Performs an inplace update of the ``"param"`` column in **df**.
@@ -176,8 +205,8 @@ def constrained_update(at1: Hashable, param: pd.Series,
 
 
 def unconstrained_update(net_charge: float, param: pd.Series, count: pd.Series,
-                         prm_min: Optional[Iterable[float]] = None,
-                         prm_max: Optional[Iterable[float]] = None,
+                         prm_min: Optional[ArrayLike] = None,
+                         prm_max: Optional[ArrayLike] = None,
                          exclude: Optional[Container[Hashable]] = None) -> None:
     """Perform an unconstrained update of atomic charges."""
     if exclude is None:
@@ -213,19 +242,140 @@ def unconstrained_update(net_charge: float, param: pd.Series, count: pd.Series,
             s = param * i
             s_clip = np.clip(s, s_min, s_max).loc[include]
 
-    # Check if the net charge is actually conserved
+    _check_net_charge(param, count, net_charge)
+
+
+def _check_net_charge(param: pd.Series, count: pd.Series, net_charge: float,
+                      tolerance: float = 0.001) -> None:
+    """Check if the net charge is actually conserved."""
     net_charge_new = get_net_charge(param, count)
-    if abs(net_charge - net_charge_new) > 0.001:
-        msg = ("Failed to conserve the net charge: "
-               f"ref = {net_charge:.4f}); {net_charge_new:.4f} != ref")
-        raise ChargeError(msg, reference=net_charge, value=net_charge_new, tol=0.001)
+    condition = abs(net_charge - net_charge_new) > tolerance
+
+    if not condition:
+        return
+
+    raise ChargeError(
+        f"Failed to conserve the net charge: ref = {net_charge:.4f}); {net_charge_new:.4f} != ref",
+        reference=net_charge, value=net_charge_new, tol=tolerance
+    )
 
 
-def invert_partial_ufunc(ufunc: functools.partial) -> functools.partial:
-    """Invert a NumPy universal function embedded within a :class:`functools.partial` instance."""
+class ChargeMapping(Mapping[FrozenSet[str], pd.DataFrame]):
+
+    __slots__ = ('__weakref__', '_data')
+
+    def __init__(self, *atom_sets: Iterable[str],
+                 constants: Optional[Iterable[str]] = None,
+                 **atoms: Tuple[float, float, float]) -> None:
+        r"""Initialize an instance."""
+        df = pd.DataFrame(atoms, columns=['values', 'min', 'max'])
+        df['values'] *= df['coefficient']
+        df['variables'] = True
+        if constants is not None:
+            df.loc[list(constants), 'variables'] = False
+
+        # Ensure that all key-sets are disjoint with respect to each other
+        keys = [frozenset(i) for i in atom_sets]
+        keys_len = sum(len(k) for k in keys)
+        try:
+            union = frozenset.union(*keys)
+        except TypeError as ex:
+            raise TypeError("missing argument '*atom_sets;") from ex
+        if keys_len != len(union):
+            raise ValueError
+
+        self._data: Mapping[FrozenSet[str], pd.DataFrame] = MappingProxyType({
+            k: df.loc[list(k)] for k in keys
+        })
+
+        # Ensure that each block has, at least, a single variable
+        for df in self.values():
+            if not df['variables'].any():
+                raise ValueError
+
+    def __getitem__(self, key: Union[str, FrozenSet[str]]) -> pd.DataFrame:
+        """Implement :meth:`self[key]<object.__getitem__>`."""
+        if isinstance(key, str):
+            for k, v in self.items():
+                if key in k:
+                    return v
+            else:
+                raise KeyError(key)
+        else:
+            return self._data[key]
+
+    def __iter__(self) -> Iterator[FrozenSet[str]]:
+        """Implement :func:`iter(self)<iter>`."""
+        return iter(self._data)
+
+    def __len__(self) -> int:
+        """Implement :func:`len(self)<len>`."""
+        return len(self._data)
+
+    def __contains__(self, key: Hashable) -> bool:
+        """Implement :meth:`key in self<object.__contains__>`."""
+        return key in self._data
+
+    def keys(self) -> KeysView[FrozenSet[str]]:
+        """Return a set-like object providing a view of this instance's keys."""
+        return self._data.keys()  # type: ignore
+
+    def items(self) -> ItemsView[FrozenSet[str], pd.DataFrame]:
+        """Return a set-like object providing a view of this instance's key/value pairs."""
+        return self._data.items()  # type: ignore
+
+    def values(self) -> ValuesView[pd.DataFrame]:
+        """Return an object providing a view of this instance's values."""
+        return self._data.values()
+
+    @overload
+    def get(self, key: Hashable) -> Optional[pd.DataFrame]:
+        ...
+    @overload
+    def get(self, key: Hashable, default: T) -> Union[pd.DataFrame, T]:
+        ...
+    def get(self, key, default=None):  # noqa: E301
+        """Return the value for **key** if it's available; return **default** otherwise."""
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __call__(self, name: str, value: float) -> pd.Series:
+        df_ref = self[name]
+        value_old = df_ref[name, 'values']
+        factor = value / value_old
+
+        df_ref.loc[df_ref['variables'], 'values'] *= factor
+        df_ref.at[name, 'values'] = value
+        df_ref['values'].clip(df_ref['min'], df_ref['max'], inplace=True)
+        charge_ref = df_ref['values'].sum()
+
+        df_iter = (df for df in self.values() if df is not df_ref)
+        for df in df_iter:
+            variables = df['variables']
+            df.loc[variables, 'values'] *= factor
+            df['values'].clip(df['min'], df['max'], inplace=True)
+            charge = df['values'].sum()
+
+            delta = charge - charge_ref
+            if np.allclose(delta, 0.0):
+                continue
+            variables = variables.copy()
+            variables &= df['values'] != df['max'] | df['values'] != df['min']
+
+            x = (charge_ref - df.loc[~variables, 'values'].sum())
+            x /= df.loc[variables, 'values'].sum()
+            df.loc[variables, 'values'] *= x
+            if not np.allclose(df['values'].sum(), charge_ref):
+                raise RuntimeError
+
+
+def invert_partial_ufunc(ufunc: partial) -> partial:
+    """Invert a NumPy universal function embedded within a :class:`partial` instance."""
     func = ufunc.func
     x2 = ufunc.args[0]
-    return functools.partial(func, x2**-1)
+    return partial(func, x2**-1)
 
 
 ExtremiteDict = Dict[Tuple[str, str], float]
@@ -302,9 +452,9 @@ def _find_float(iterable: Tuple[str, str]) -> Tuple[str, float]:
         return i, float(j)
 
 
-def _eq_constraints(constrain_: list) -> Dict[str, functools.partial]:
+def _eq_constraints(constrain_: list) -> Dict[str, partial]:
     """Parse :math:`a = i * b`-type constraints."""
-    constrain_dict: Dict[str, functools.partial] = {}
+    constrain_dict: Dict[str, partial] = {}
     constrain = ''.join(str(i) for i in constrain_).split('==')
     iterator: Iterator[str] = iter(constrain)
 
@@ -316,12 +466,12 @@ def _eq_constraints(constrain_: list) -> Dict[str, functools.partial]:
     elif len(item_) == 2:
         atom, multiplier = _find_float(item_)
         multiplier **= -1
-    constrain_dict[atom] = functools.partial(np.multiply, 1.0)
+    constrain_dict[atom] = partial(np.multiply, 1.0)
 
     # Assign all other constraints
     for item in iterator:
         item_ = item.split('*')
         atom, i = _find_float(item_)
         i *= multiplier
-        constrain_dict[atom] = functools.partial(np.multiply, i)
+        constrain_dict[atom] = partial(np.multiply, i)
     return constrain_dict
